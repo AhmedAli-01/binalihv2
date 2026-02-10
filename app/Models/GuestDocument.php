26c3c9cff109d3e21fa73f1b62e7b3b655e9195a
AhmedAli-01 <15903693+AhmedAli-01@users.noreply.github.com>
2026-02-10 16:28:27 +0000

    freshly instaled laravel + tables + migratoin + factory all freshh and empty

---
+++ app/Models/GuestDocument.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GuestDocument extends Model
+{
+    /** @use HasFactory<\Database\Factories\GuestDocumentFactory> */
+    use HasFactory;
+}
---
+++ app/Models/HousekeepingTask.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HousekeepingTask extends Model
+{
+    /** @use HasFactory<\Database\Factories\HousekeepingTaskFactory> */
+    use HasFactory;
+}
